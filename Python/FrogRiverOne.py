5c0dbce5fbb17e1d1a9112a4713b48c2f8dcaec1
Wagner Macedo <wagnerluis1982@gmail.com>
2023-12-08 22:42:38 +0100

    Create FrogRiverOne.py

---
+++ Python/FrogRiverOne.py
@@ -0,0 +1,14 @@
+def solution(X, A):
+    # Implement your solution here
+    positions = set(e for e in A if e <= X)
+
+    for P in range(1, X + 1):
+        if P not in positions:
+            return -1
+
+    for K in range(len(A)):
+        positions.discard(A[K])
+        if len(positions) == 0:
+            return K
+
+    return -1
