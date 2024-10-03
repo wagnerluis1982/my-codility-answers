ab06634aa4c9de29ed809216ffada26f77f9e8cc
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-03 23:29:12 +0200

    Update CountDiv.py

--- Python/CountDiv.py
+++ Python/CountDiv.py
@@ -1,10 +1,11 @@
-# https://app.codility.com/demo/results/trainingAMKBW9-GQU/
+# https://app.codility.com/demo/results/training57WSW4-EWM/
 
-# Task Score   62%
-# Correctness 100%
-# Performance  25%
+# Task Score   100%
+# Correctness  100%
+# Performance  100%
 
 def solution(A: int, B: int, K: int) -> int:
+    # first number divisible
     for i in range(A, B + 1):
         if i % K == 0:
             A = i
@@ -12,8 +13,10 @@ def solution(A: int, B: int, K: int) -> int:
     else:
         return 0
 
-    count = 0
-    for i in range(A, B + 1, K):
-        count += 1
+    # last number divisible
+    for i in reversed(range(A, B + 1)):
+        if i % K == 0:
+            B = i
+            break
 
-    return count
+    return (B - A) // K + 1
