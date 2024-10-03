cc423fbbae34878939f93d974dc82d4dca2e15dd
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-03 23:17:07 +0200

    Update CountDiv.py

--- Python/CountDiv.py
+++ Python/CountDiv.py
@@ -1,8 +1,19 @@
-# https://app.codility.com/demo/results/trainingBZB2W8-RUF/
+# https://app.codility.com/demo/results/trainingAMKBW9-GQU/
 
-# Task Score   50%
+# Task Score   62%
 # Correctness 100%
-# Performance   0%
+# Performance  25%
 
-def solution(A, B, K):
-    return len({i for i in range(A, B + 1) if i % K == 0})
+def solution(A: int, B: int, K: int) -> int:
+    for i in range(A, B + 1):
+        if i % K == 0:
+            A = i
+            break
+    else:
+        return 0
+
+    count = 0
+    for i in range(A, B + 1, K):
+        count += 1
+
+    return count
