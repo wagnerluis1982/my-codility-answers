bed81cc63464d39560714f4635e4178956bb166d
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-03 22:44:08 +0200

    Update PassingCars.py

--- Python/PassingCars.py
+++ Python/PassingCars.py
@@ -1,24 +1,22 @@
-# https://app.codility.com/demo/results/training8DJ5HX-UWE/
+# https://app.codility.com/demo/results/trainingAD46UU-SNQ/
 
-# Task Score    90%
-# Correctness   80%
+# Task Score   100%
+# Correctness  100%
 # Performance  100%
 
 THRESHOLD = 1_000_000_000
 
 def solution(A: list) -> int:
-    # Implement your solution here
     if len(A) == 1:
         return 0
 
+    inc = 0
     count = 0
-    inc = 1
-    start = A[0]
 
-    for i in range(1, len(A)):
-        if A[i] == start:
+    for i in range(0, len(A)):
+        if A[i] == 0:
             inc += 1
-        else:
+        elif inc > 0:
             count += inc
             if count > THRESHOLD:
                 return -1
