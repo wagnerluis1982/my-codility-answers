fe33de6d7b26f0e6d2c247c78fbf13b94ff63a28
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-08 22:06:58 +0200

    Update Dominator.py

--- Python/Dominator.py
+++ Python/Dominator.py
@@ -1,24 +1,28 @@
-# https://app.codility.com/demo/results/training6SB4HF-UA3/
+# https://app.codility.com/demo/results/trainingMVCZDC-4TS/
 
 # Task Score   100%
 # Correctness  100%
 # Performance  100%
 
+from collections import defaultdict
+
 def solution(A: list) -> int:
-    if not A:
+    N = len(A)
+    if N == 0 or N == 2:
         return -1
+    if N == 1:
+        return 0
 
-    count = {}
     index = {}
-    for i in range(len(A)):
-        value = A[i]
-        if value not in count:
-            count[value] = 1
-            index[value] = i
-        else:
-            count[value] += 1
+    count = defaultdict(int)
 
-        if count[value] > len(A) // 2:
+    for i in range(N):
+        value = A[i]
+        if value not in index:
+            index[value] = i
+
+        count[value] += 1
+        if count[value] > N // 2:
             return index[value]
 
     return -1
