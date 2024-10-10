d9de5aa1fd35e68120dd485d5346839389ffb3b6
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-10 19:01:24 +0200

    GenomicRangeQuery.py historic v4

--- Python/Lessons/L5/GenomicRangeQuery.py
+++ Python/Lessons/L5/GenomicRangeQuery.py
@@ -1,27 +1,20 @@
-# https://app.codility.com/demo/results/trainingJHUPW6-A2X/
+# https://app.codility.com/demo/results/trainingSX237A-2CJ/
 
 # Task Score    62%
 # Correctness  100%
 # Performance    0%
 
 def solution(S: str, P: list, Q: list) -> list:
-    return [calc_impact(S, P, Q, k) for k in range(min((len(P), len(Q))))]
+    T = [impact(s) for s in S]
+    return [min(T[P[k]:Q[k]+1]) for k in range(min((len(P), len(Q))))]
 
 
-def calc_impact(S: str, P: list, Q: list, k: int) -> int:
-    nucleotides = set()
-    for i in range(P[k], Q[k] + 1):
-        nct = S[i]
-        if nct == 'A':
-            return 1
-
-        nucleotides.add(nct)
-        if len(nucleotides) == 4:
-            return 1
-
-    if 'C' in nucleotides:
+def impact(s: str) -> int:
+    if s == 'A':
+        return 1
+    elif s == 'C':
         return 2
-    elif 'G' in nucleotides:
+    elif s == 'G':
         return 3
     else:
         return 4
