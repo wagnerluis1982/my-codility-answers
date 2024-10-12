dd44fed4b7082c252450413af803d4a41f2cdf6e
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-12 22:32:43 +0200

    Update MinAvgTwoSlice.py

--- Python/Lessons/L5/MinAvgTwoSlice.py
+++ Python/Lessons/L5/MinAvgTwoSlice.py
@@ -11,12 +11,16 @@ def solution(A: list) -> int:
     for k in range(1, N + 1):
         S[k] = S[k - 1] + A[k - 1]
 
+    started = set()
     index = 0
     lowest = slice(S, 0, 1)
-    for p in range(0, N - 1):
-        for q in range(p + 1, N):
-            if slice(S, p, q) < lowest:
-                lowest = slice(S, p, q)
+    for p in range(N - 1):
+        q = p + 1
+        started.add(p)
+        for p in started:
+            avg = slice(S, p, q)
+            if avg < lowest:
+                lowest = avg
                 index = p
 
     return index
