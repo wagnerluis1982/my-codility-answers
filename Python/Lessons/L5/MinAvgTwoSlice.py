cf48e62df31a4543e5eff06b5b50365bc8dca233
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-12 19:52:15 +0200

    Update MinAvgTwoSlice.py

--- Python/Lessons/L5/MinAvgTwoSlice.py
+++ Python/Lessons/L5/MinAvgTwoSlice.py
@@ -1,47 +1,26 @@
-# https://app.codility.com/demo/results/trainingV8H46U-VFN/
+# https://app.codility.com/demo/results/trainingPV5YF2-VSJ/
 
-# Task Score    50%
+# Task Score    60%
 # Correctness  100%
-# Performance    0%
+# Performance   20%
 
 def solution(A: list) -> int:
     N = len(A)
-    P = prefix_sums(A, N)
-    V = prefix_averages(P, N)
+
+    S = [0] * (N + 1)
+    for k in range(1, N + 1):
+        S[k] = S[k - 1] + A[k - 1]
 
     index = 0
-    lesser = V[0][1]
-    x, y = 0, 2
-    while y < N:
-        if V[x][y] < lesser:
-            lesser = V[x][y]
-            index = x
-
-        y += 1
-        if y >= N:
-            x, y = x + 1, x + 2
+    lowest = slice(S, 0, 1)
+    for p in range(0, N - 1):
+        for q in range(p + 1, N):
+            if slice(S, p, q) < lowest:
+                lowest = slice(S, p, q)
+                index = p
 
     return index
 
 
-def prefix_sums(A: list, N: int) -> list:
-    P = [0] * (N + 1)
-    for k in range(1, N + 1):
-        P[k] = P[k - 1] + A[k - 1]
-    return P
-
-
-def prefix_averages(P: list, N: int) -> list:
-    V = [[0] * N for _ in range(N)]
-    for i in range(0, N - 1):
-        for j in range(i + 1, N):
-            V[i][j] = slice(P, i, j)
-    return V
-
-def slice(P: list, x: int, y: int) -> float:
-    if x > y:
-        raise Exception(f"Invalid range: x={x} > y={y}")
-    try:
-        return (P[y + 1] - P[x]) / (y - x + 1)
-    except IndexError:
-        raise Exception(f"Invalid index: x={x}, y+1={y + 1}")
+def slice(S: list, x: int, y: int) -> float:
+    return (S[y + 1] - S[x]) / (y - x + 1)
