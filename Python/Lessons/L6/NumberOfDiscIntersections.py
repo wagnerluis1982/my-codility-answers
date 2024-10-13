b6c5fdc2c07e70d41eee5804af5d694080ff644a
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-13 11:21:48 +0200

    Update NumberOfDiscIntersections.py

--- Python/Lessons/L6/NumberOfDiscIntersections.py
+++ Python/Lessons/L6/NumberOfDiscIntersections.py
@@ -1,18 +1,24 @@
-# https://app.codility.com/demo/results/trainingS27JHX-AFX/
+# https://app.codility.com/demo/results/training2V594V-THF/
 
 # Task Score    12%
 # Correctness   25%
 # Performance    0%
 
+# [1, 1, 1] ~> got 1 expected 3
+
 THRESHOLD = 10_000_000
 
 def solution(A: list) -> int:
     N = len(A)
+
     intersects = set()
-    for J in range(N):
-        for K in range(max(0, J - A[J]), min(N, J + A[J] + 1)):
-            if K != J:
-                intersects.add(tuple(sorted([J, K])))
+    for j in range(0, N - 1):
+        if A[j] > 0:
+            for k in range(max(0, j - A[j]), min(N, j + A[j])):
+                if j == k:
+                    continue
+                point = (j, k) if j < k else (k, j)
+                intersects.add(point)
                 if len(intersects) > THRESHOLD:
                     return -1
 
