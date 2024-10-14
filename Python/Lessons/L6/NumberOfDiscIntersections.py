2e493ee9d6642715df145f6b099844d27f2495ae
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-14 06:39:09 +0200

    Update NumberOfDiscIntersections.py

--- Python/Lessons/L6/NumberOfDiscIntersections.py
+++ Python/Lessons/L6/NumberOfDiscIntersections.py
@@ -1,4 +1,4 @@
-# https://app.codility.com/demo/results/trainingJ2TKMW-WYB/
+# https://app.codility.com/demo/results/trainingHRQ7SW-4AY/
 
 # Task Score    56%
 # Correctness  100%
@@ -9,22 +9,17 @@ THRESHOLD = 10_000_000
 
 def solution(A: list) -> int:
     N = len(A)
-    intersects = [set() for _ in range(N)]
-    for j in range(N):
-        for k in range(max(0, j - A[j]), min(N, j + A[j] + 1)):
-            if j != k:
-                intersects[k].add(j)
 
     pairs = set()
-    for j, points in enumerate(intersects):
-        for k in points:
-            pairs.add((j, k) if j < k else (k, j))
-            if len(pairs) > THRESHOLD:
-                return -1
-            for l in points:
-                if k != l:
-                    pairs.add((k, l) if k < l else (l, k))
-                    if len(pairs) > THRESHOLD:
-                        return -1
+    intersects = [{p} for p in range(N)]
+    for p in range(N):
+        for q in range(max(0, p - A[p]), min(N, p + A[p] + 1)):
+            if p != q:
+                intersects[q].add(p)
+                for r in intersects[q]:
+                    if p != r:
+                        pairs.add((p, r) if p < r else (r, p))
+                        if len(pairs) > THRESHOLD:
+                            return -1
 
     return len(pairs)
