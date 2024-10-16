12e984d61eaf31b3d1df8c1a3072e7e5433f4d93
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-16 06:56:12 +0200

    Update MaxProfit.py

--- Python/Lessons/L9/MaxProfit.py
+++ Python/Lessons/L9/MaxProfit.py
@@ -1,4 +1,4 @@
-# https://app.codility.com/demo/results/training6X3EMS-G5E/
+# https://app.codility.com/demo/results/trainingFNUV3Z-2ES/
 
 # Task Score   100%
 # Correctness  100%
@@ -8,24 +8,10 @@ def solution(A: list) -> int:
     if len(A) < 2:
         return 0
 
-    low = high = A[0]
+    cheapest = A[0]
     profit = 0
-    low_idx = high_idx = 0
     for i in range(1, len(A)):
-        share = A[i]
-
-        if share > high:
-            high = share
-            high_idx = i
-
-        if share < low:
-            low = share
-            low_idx = i
-
-        if low_idx <= high_idx:
-            profit = max(profit, high - low)
-        else:
-            high = low
-            high_idx = low_idx
+        cheapest = min(cheapest, A[i])
+        profit = max(profit, A[i] - cheapest)
 
     return profit
