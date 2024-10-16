5160f550440c8e211146c032cde8d4277ffce312
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-16 06:00:53 +0200

    Update NumberOfDiscIntersections.py

--- Python/Lessons/L6/NumberOfDiscIntersections.py
+++ Python/Lessons/L6/NumberOfDiscIntersections.py
@@ -1,26 +1,36 @@
-# https://app.codility.com/demo/results/trainingK5SDJ3-SVZ/
+# https://app.codility.com/demo/results/trainingEYSCNB-2KB/
 
 # Source: https://github.com/dudmy/study/blob/master/Codility/NumberOfDiscIntersections.md
 
-# Task Score    56%
+# Task Score   100%
 # Correctness  100%
-# Performance   12%
+# Performance  100%
 
 THRESHOLD = 10_000_000
 
 
 def solution(A: list) -> int:
     count = 0
-    for p in range(len(A)):
-        J = p + A[p]
 
-        for q in range(p + 1, len(A)):
-            K = q - A[q]
+    right = []
+    left = []
+    for i in range(len(A)):
+        right.append(i + A[i])
+        left.append(i - A[i])
+    right.sort()
+    left.sort()
 
-            if J >= K:
-                count += 1
+    j = 0
+    k = 0
+    while j < len(A) and k < len(A):
+        while k < len(A) and right[j] >= left[k]:
+            count += k - j
 
             if count > THRESHOLD:
                 return -1
+
+            k += 1
+
+        j += 1
 
     return count
