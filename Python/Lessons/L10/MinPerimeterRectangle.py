b76102f97e046e84ab61a6c33a66592a3301341f
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-17 20:30:31 +0200

    Updated MinPerimeterRectangle.py

--- Python/Lessons/L10/MinPerimeterRectangle.py
+++ Python/Lessons/L10/MinPerimeterRectangle.py
@@ -1,15 +1,13 @@
-# https://app.codility.com/demo/results/training8GYEHJ-JUD/
+# https://app.codility.com/demo/results/trainingVHTHVV-2BF/
 
-# Task Score    80%
-# Correctness   80%
-# Performance   80%
-
-# 36 ~> got 26 expected 24
+# Task Score   100%
+# Correctness  100%
+# Performance  100%
 
 def solution(N: int) -> int:
     min_perimeter = 2 * (1 + N)
     t = 2
-    while t ** 2 < N:
+    while t ** 2 <= N:
         if N % t == 0:
             min_perimeter = min(min_perimeter, 2 * (t + N // t))
         t += 1
