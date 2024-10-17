f54cd3d2ac5cbf50eb271d1c26acd84aa96ee6a4
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-17 06:51:00 +0200

    Update MaxSliceSum.py

--- Python/Lessons/L9/MaxSliceSum.py
+++ Python/Lessons/L9/MaxSliceSum.py
@@ -1,15 +1,13 @@
-# https://app.codility.com/demo/results/trainingCT69D9-GXY/
+# https://app.codility.com/demo/results/trainingD249XU-85E/
 
-# Task Score    69%
-# Correctness   62%
-# Performance   80%
-
-# [-2, 1] ~> got 0 expected 1
+# Task Score   100%
+# Correctness  100%
+# Performance  100%
 
 def solution(A: list) -> int:
-    max_slice = A[0]
-    max_ending = max_slice
+    max_slice = max_ending
+    max_ending = A[0]
     for i in range(1, len(A)):
-        max_ending = max(0, max_ending + A[i])
+        max_ending = max(A[i], max_ending + A[i])
         max_slice = max(max_slice, max_ending)
     return max_slice
