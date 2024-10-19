efca617710a6f89a5244df2ee4cca22576dab546
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-19 09:22:25 +0200

    Updated ChocolatesByNumbers.py

--- Python/Lessons/L12/ChocolatesByNumbers.py
+++ Python/Lessons/L12/ChocolatesByNumbers.py
@@ -1,15 +1,28 @@
-# https://app.codility.com/demo/results/training7VQCVD-NAE/
+# https://app.codility.com/demo/results/trainingRKQ338-UBX/
 
 # Task Score    62%
-# Correctness  100%
-# Performance   25%
+# Correctness   50%
+# Performance   75%
+
+# [12, 21] ~> got 12 expected 4
+
+import math
+
 
 def solution(N: int, M: int) -> int:
-    eaten = set()
+    if N == M:
+        return 1
 
-    x = 0
-    while x not in eaten:
-        eaten.add(x)
-        x = (x + M) % N
+    if N < M:
+        if M % N == 0:
+            return 1
+    else:
+        if N % M == 0:
+            return N // M
+        elif N % 2 == 0 and M % 2 == 0:
+            return N // math.gcd(N, M)
 
-    return len(eaten)
+    if M % 2 == 1:
+        return N
+
+    return math.gcd(N, M)
