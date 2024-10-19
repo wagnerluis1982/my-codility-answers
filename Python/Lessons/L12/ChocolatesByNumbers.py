693eeed537378dd71564c41e7000b43cfa65420d
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-19 09:43:06 +0200

    Updated ChocolatesByNumbers.py

--- Python/Lessons/L12/ChocolatesByNumbers.py
+++ Python/Lessons/L12/ChocolatesByNumbers.py
@@ -1,28 +1,10 @@
-# https://app.codility.com/demo/results/training2FXW4J-S58/
+# https://app.codility.com/demo/results/trainingA3C4W9-ZTR/
 
-# Task Score    87%
-# Correctness   75%
+# Task Score   100%
+# Correctness  100%
 # Performance  100%
 
-from math import gcd
-
+import math
 
 def solution(N: int, M: int) -> int:
-    if N == M:
-        return 1
-
-    if N < M:
-        if M % N == 0:
-            return 1
-        else:
-            return N // gcd(N, M)
-    else:
-        if N % M == 0:
-            return N // M
-        elif N % 2 == 0 and M % 2 == 0:
-            return N // gcd(N, M)
-
-    if M % 2 == 1:
-        return N
-
-    return gcd(N, M)
+    return N // math.gcd(N, M)
