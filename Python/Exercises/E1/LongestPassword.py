2ff3a2f2b17e536993f1d968047274cec0f1b11e
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-19 17:22:55 +0200

    Exercise 1: LongestPassword.py

---
+++ Python/Exercises/E1/LongestPassword.py
@@ -0,0 +1,28 @@
+# https://app.codility.com/demo/results/trainingWCCP2R-AE7/
+
+# Task Score    60%
+# Correctness   60%
+# Performance    -
+
+def solution(S: str):
+    max_password = -1
+    state = ""
+    letters = 0
+    digits = 0
+    for c in S:
+        if c.isspace():
+            if state == "PW" and letters % 2 == 0 and digits % 2 == 1:
+                max_password = max(max_password, letters + digits)
+            state = "SP"
+            letters = 0
+            digits = 0
+        elif not c.isalnum():
+            state = "BAD"
+        elif state != "BAD":
+            state = "PW"
+
+        if state == "PW":
+            digits += c.isdigit()
+            letters += c.isalpha()
+
+    return max_password
