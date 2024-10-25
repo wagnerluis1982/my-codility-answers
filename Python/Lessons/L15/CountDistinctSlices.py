e8b14e847753a51180116bb04ae6cff312c95d81
Wagner Macedo <wagnerluis1982@gmail.com>
2024-10-25 08:15:04 +0200

    Updated CountDistinctSlices.py

--- Python/Lessons/L15/CountDistinctSlices.py
+++ Python/Lessons/L15/CountDistinctSlices.py
@@ -1,17 +1,29 @@
-# https://app.codility.com/demo/results/training35HVFA-Y38/
+# https://app.codility.com/demo/results/trainingT8UU2Z-9TD/
 
 # Task Score    30%
 # Correctness   40%
 # Performance   20%
 
 def solution(M: int, A: list) -> int:
+    N = len(A)
     B = set()
     count = 0
-    for value in A:
-        if value in B:
+
+    p = 0
+    q = 0
+    while p <= q < N:
+        if A[q] in B:
             count += count_pairs(B)
+            while p <= q and A[p] != A[q]:
+                B.discard(A[p])
+                p += 1
+            B.discard(A[p])
+            count += max(0, q - p - 1)
+            p = q
             B.clear()
-        B.add(value)
+        B.add(A[q])
+        q += 1
+
     count += count_pairs(B)
     return count
 
